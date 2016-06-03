ce562f72659efe5bc07c4f1ce932fa4577c72165
RobStek <rob.stekelenburg@idgis.nl>
2016-06-03 11:06:59 +0200

    removed awstats conf

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ service apache2 start
 
 while [ true ] ; 
 do
-/usr/bin/perl /usr/lib/cgi-bin/awstats/awstats.pl -config=linoxide -update
 /usr/bin/perl /usr/lib/cgi-bin/awstats/awstats.pl -config=stats -update
 sleep 600
 done
